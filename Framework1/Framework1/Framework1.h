289d31fa6c392433e3231ca02e73c96d5e7110a0
vit-sumit-rathore <sumit.rathore@virtuosoitech.com>
2021-07-27 13:09:08 +0530

    Initial setup

---
+++ Framework1/Framework1/Framework1.h
@@ -0,0 +1,18 @@
+//
+//  Framework1.h
+//  Framework1
+//
+//  Created by VIT27 on 27/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Framework1.
+FOUNDATION_EXPORT double Framework1VersionNumber;
+
+//! Project version string for Framework1.
+FOUNDATION_EXPORT const unsigned char Framework1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework1/PublicHeader.h>
+
+
